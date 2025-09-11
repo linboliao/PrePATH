a11a5484ad6911df5a0b45d827daaeafbce4c590
linboliao <linboliao123@163.com>
2025-09-11 15:23:24 +0800

    迈新：补充切patches 时分割参数注释；

--- scripts/get_coors/creat_patches_fp.sh
+++ scripts/get_coors/creat_patches_fp.sh
@@ -1,20 +1,12 @@
 # The coordinates of the patches are saved, you can change the path to any directory you want.
-save_dir="/jhcnas3/Pathology/code/PrePath/temp/patches"
+save_dir="/NAS2/Data1/lbliao/Data/MXB/classification/测试一/yolo_patches"
 # The directory where the WSI files are stored, you can change it to any directory you want.
-wsi_dir="/jhcnas3/Pathology/code/PrePath/temp/svs"
+wsi_dir="/NAS2/Data1/lbliao/Data/MXB/classification/测试一/slides"
 # The WSI format, you can set it based on the format of your WSI
-wsi_format="svs"
+wsi_format="kfb"
 # The log file name, you can change it to any name you want.
 log_name="SAL.log"
-
+cd ../../
 # Normally, you don't need to change following lines.
 # to set the patch size, please set it at `configs/resolution.py`
-python create_patches_fp.py \
-        --source $wsi_dir \
-        --save_dir $save_dir\
-        --preset tcga.csv \
-        --patch_level 0 \
-        --wsi_format $wsi_format \
-        --seg \
-        --patch \
-        --stitch
+python create_patches_fp.py --source $wsi_dir --save_dir $save_dir --patch_size 2048 --step_size 2048 --preset maixin.csv --patch_level 0 --wsi_format $wsi_format --seg --patch --stitch --use_mp --no_auto_skip
