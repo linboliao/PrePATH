3ca873997549eed743dde6c47248079cdea0b7c5
Yjing07 <jingy_7@163.com>
2026-03-12 09:11:04 +0000

    协和：更新代码

--- scripts/get_coors/create_patches_fp.sh
+++ scripts/get_coors/create_patches_fp.sh
@@ -6,9 +6,10 @@
 #python create_patches_fp.py --source $wsi_dir --save_dir $save_dir --patch_size $patch_size --step_size $patch_size --preset xiehe.csv --patch_level 0 --wsi_format $wsi_format --seg --patch --stitch --use_mp
 cd ../../
 
-patch_size=448
-save_dir=/NAS145/Data/MXB/子品牌图片/patches_0_$patch_size
-wsi_dir=/NAS145/Data/MXB/子品牌图片
-wsi_format="svs"
+patch_size=224
+patch_level=0
+save_dir=/NAS145/Data/MXB/CLS/patches_0_$patch_size
+wsi_dir=/NAS145/Data/MXB/CLS
+wsi_format="svs;kfb"
 
-python create_patches_fp.py --source $wsi_dir --save_dir $save_dir --patch_size $patch_size --step_size $patch_size --preset xiehe.csv --patch_level 0 --wsi_format $wsi_format --seg --patch --stitch --use_mp
+python create_patches_fp_mag.py --source $wsi_dir --save_dir $save_dir --patch_size $patch_size --step_size $patch_size --preset xiehe.csv --patch_level $patch_level --wsi_format $wsi_format --seg --patch --stitch --use_mp
